614ca7121bd3aec534df23c6acd828b1e90e49f3
MuhammadMudabbiMunaf <christopherhouston222@gmail.com>
2023-01-03 23:28:41 +0500

    my server code

---
+++ server.mjs
@@ -0,0 +1,17 @@
+console.log("I am a server file");
+
+import express from "express";
+// const express = require('express')   old method
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+
+  console.log("request ip: ", req.ip)
+
+  res.send('Hello World!' + new Date().toString());
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
